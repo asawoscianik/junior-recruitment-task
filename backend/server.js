2f010241584390771de12d56eaf47b82aa33bc4a
Adam Sawoscianik <a.sawoscianik@gmail.com>
2018-01-07 21:37:22 +0100

    fix formating

--- backend/server.js
+++ backend/server.js
@@ -6,41 +6,38 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 //create database
-db.serialize(function() {
+db.serialize(function () {
     db.run("CREATE TABLE IF NOT EXISTS tasks (task TEXT, isdone INTEGER)");
 });
 
-
 //handle with error(no 'Access-Control-Allow-Origin') 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
-
 //insert values from input to database
-app.post('/tasks', function(req, res){
+app.post('/tasks', function (req, res) {
 
     var stmt = db.prepare('INSERT INTO tasks VALUES (?,?)');
-    if(req.body.task!=""){
-    stmt.run(req.body.task, req.body.isdone);
-    stmt.finalize();
+    if (req.body.task != "") {
+        stmt.run(req.body.task, req.body.isdone);
+        stmt.finalize();
     }
-    else{
+    else {
         console.log('empty field')
     }
 
     db.each('SELECT * FROM tasks', function (err, row) {
 
-        if (err){
-            console.log("Failed to insert.");             
+        if (err) {
+            console.log("Failed to insert.");
         }
 
-        else{
+        else {
             res.end();
         }
 
@@ -49,39 +46,30 @@ app.post('/tasks', function(req, res){
 });
 
 //create json
-app.get('/tasks', function(req, res){
-    db.all("SELECT * FROM tasks", function(err, rows){
+app.get('/tasks', function (req, res) {
+    db.all("SELECT * FROM tasks", function (err, rows) {
         res.json(rows);
-        res.end() 
-   });
+        res.end()
+    });
 });
-
-
-app.post('/delete',function(req, res){
+//delete from database
+app.post('/delete', function (req, res) {
     var stmt = db.prepare(`DELETE FROM tasks WHERE task=?`);
     stmt.run(req.body.delete);
     stmt.finalize();
 });
-
-app.post('/update1',function(req, res){
+//update when checked
+app.post('/update1', function (req, res) {
     var stmt = db.prepare(`UPDATE tasks SET isdone = 1 WHERE task=?`);
     stmt.run(req.body.update);
     stmt.finalize();
 });
-
-app.post('/update2',function(req, res){
+//update when uchecked
+app.post('/update2', function (req, res) {
     var stmt = db.prepare(`UPDATE tasks SET isdone = 0 WHERE task=?`);
     stmt.run(req.body.update);
     stmt.finalize();
 });
-
-// let text = 4;
-// db.run(`DELETE FROM tasks WHERE rowid=?`, text, function(err) {
-//     if (err) {
-//         return console.error(err.message);
-//     }
-//     console.log(`Row deleted`);
-// });
 
 app.listen(3000);
 
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,146 +1,130 @@
-window.onload=function(){
+window.onload = function () {
     let count = 0;
     let addTaskBtn = document.getElementById('add_btn');
     let addTaskInput = document.getElementById('add_task');
-    let container = document.getElementById('container'); 
-    let task_get ="";
-    
-    //if checkbox is checked add class done and post changes to server else remove class done and post changes to server
-    function addDoneClass(){
+    let container = document.getElementById('container');
+    let task_get = "";
 
-        $(document).on('change', '.checkbox_task', function (){
+    //if checkbox is checked add class done and post changes to server else remove class done and post changes to server
+    function addDoneClass() {
+
+        $(document).on('change', '.checkbox_task', function () {
 
             let parent = $(this).parent().parent();
 
-            if(this.checked){
+            if (this.checked) {
 
                 let data = {};
                 data.update = parent.text();
 
                 $.ajax({
                     type: 'POST',
-                    headers: {"Content-Type": "application/json"},
-                    url: 'http://localhost:3000/update1',	
-                    data: JSON.stringify(data),                     					
-                    success: function(res) {
-                        // console.log(res);
-                        // console.log(JSON.stringify(data));
-                        // console.log('Succes')
-                    },
-                    error: function(res, err){
-                        // console.log(res);
-                        // console.log(err);
+                    headers: { "Content-Type": "application/json" },
+                    url: 'http://localhost:3000/update1',
+                    data: JSON.stringify(data),
+                    error: function (err) {
+                        console.log(err);
                     }
                 });
 
                 parent.addClass("done");
+
             }
 
-            else{
+            else {
 
                 let data = {};
                 data.update = parent.text();
 
                 $.ajax({
                     type: 'POST',
-                    headers: {"Content-Type": "application/json"},
-                    url: 'http://localhost:3000/update2',	
-                    data: JSON.stringify(data),                     					
-                    success: function(res) {
-                        // console.log(res);
-                        // console.log(JSON.stringify(data));
-                        // console.log('Succes')
-                    },
-                    error: function(res, err){
-                        // console.log(res);
-                        // console.log(err);
+                    headers: { "Content-Type": "application/json" },
+                    url: 'http://localhost:3000/update2',
+                    data: JSON.stringify(data),
+                    error: function (err) {
+                        console.log(err);
                     }
                 });
 
                 parent.removeClass("done");
 
             }
-            
+
         });
-    }    
+    }
 
     //if input is active change color of add button
-    function changeBtnColor(){
+    function changeBtnColor() {
 
-        document.addEventListener('click', function(event) {
+        document.addEventListener('click', function (event) {
             var isClickInside = addTaskInput.contains(event.target);
-          
+
             if (isClickInside) {
                 addTaskBtn.src = "assets/add_green.png";
             }
-            else{
+            else {
                 addTaskBtn.src = "assets/add.png";
             }
-          });
-    }      
+        });
+    }
 
     //check if input is empty and when it is dont post/add task 
-    function checkIfEmpty(){
+    function checkIfEmpty() {
 
-        addTaskInput.addEventListener("keydown", function(e){
-            
-            if (e.keyCode === 13) { 
-    
-                if(addTaskInput.value !== ""){
+        addTaskInput.addEventListener("keydown", function (e) {
+
+            if (e.keyCode === 13) {
+
+                if (addTaskInput.value !== "") {
                     postTask();
                     addTask();
                 }
-                else{
+                else {
                     alert("Empty field!");
                 }
             }
         });
 
-        addTaskBtn.addEventListener("click", function() {    
+        addTaskBtn.addEventListener("click", function () {
 
-            if(addTaskInput.value !== ""){
+            if (addTaskInput.value !== "") {
                 postTask();
                 addTask();
             }
-            else{
+            else {
                 alert("Empty field!");
             }
         });
     }
 
     //add tash to html
-    function addTask(){
+    function addTask() {
 
-        let task = '<li class="content"><div class="left content_left"><input class="checkbox_task" id="checkbox_active'+count+'" type="checkbox"><label for="checkbox_active'+count+'"></label></div>'
-        + '<div class="middle conent_middle"></div>'
-        + '<div class="right content_right">' +'<div class="task_text">'+ addTaskInput.value + '</div>'+'<img class="trash_can" src="assets/trash-can-active.png"></div></li>';
+        let task = '<li class="content"><div class="left content_left"><input class="checkbox_task" id="checkbox_active' + count + '" type="checkbox"><label for="checkbox_active' + count + '"></label></div>'
+            + '<div class="middle conent_middle"></div>'
+            + '<div class="right content_right">' + '<div class="task_text">' + addTaskInput.value + '</div>' + '<img class="trash_can" src="assets/trash-can-active.png"></div></li>';
 
         container.innerHTML += task;
         addTaskInput.value = "";
         count++;
     }
 
-    //when click on trash can image delete task from db html
-    function removeTask(){
 
-        $(document).on('click', '.trash_can', function (){
+    //when click on trash can image delete task from db html
+    function removeTask() {
+
+        $(document).on('click', '.trash_can', function () {
 
             let data = {};
             data.delete = $(this).parent().text();
 
             $.ajax({
                 type: 'POST',
-                headers: {"Content-Type": "application/json"},
-                url: 'http://localhost:3000/delete',	
-                data: JSON.stringify(data),                     					
-                success: function(res) {
-                    // console.log(res);
-                    // console.log(JSON.stringify(data));
-                    // console.log('Succes')
-                },
-                error: function(res, err){
-                    // console.log(res);
-                    // console.log(err);
+                headers: { "Content-Type": "application/json" },
+                url: 'http://localhost:3000/delete',
+                data: JSON.stringify(data),
+                error: function (err) {
+                    console.log(err);
                 }
             });
 
@@ -151,44 +135,46 @@ window.onload=function(){
     }
 
     //get tasks from localhost json and insert into html
-    function getTask(){
+    function getTask() {
 
         $.ajax({
             type: 'GET',
             url: "http://localhost:3000/tasks",
 
             success: function (result) {
-                
+
                 var totalTasks = Object.keys(result).length;
 
-                for(var i =0;i<totalTasks;i++){
+                for (var i = 0; i < totalTasks; i++) {
 
-                    if(result[i].isdone==0){
+                    if (result[i].isdone == 0) {
 
-                        task_get = '<li class="content"><div class="left content_left"><input class="checkbox_task" id="checkbox_active'+count+'" type="checkbox"><label for="checkbox_active'+count+'"></label></div>'
-                        + '<div class="middle conent_middle"></div>'
-                        + '<div class="right content_right">' +'<div class="task_text">'+ result[i].task + '</div>'+'<img class="trash_can" src="assets/trash-can-active.png"></div></li>';
+                        task_get = '<li class="content"><div class="left content_left"><input class="checkbox_task" id="checkbox_active' + count + '" type="checkbox"><label for="checkbox_active' + count + '"></label></div>'
+                            + '<div class="middle conent_middle"></div>'
+                            + '<div class="right content_right">' + '<div class="task_text">' + result[i].task + '</div>' + '<img class="trash_can" src="assets/trash-can-active.png"></div></li>';
                         count++;
                     }
-                    
-                    else{
-                        task_get = '<li class="done content"><div class="left content_left"><input class="checkbox_task" id="checkbox_active'+count+'" type="checkbox" checked><label for="checkbox_active'+count+'"></label></div>'
-                        + '<div class="middle conent_middle"></div>'
-                        + '<div class="right content_right">' +'<div class="task_text">'+ result[i].task + '</div>'+'<img class="trash_can" src="assets/trash-can-active.png"></div></li>';
+
+                    else {
+                        task_get = '<li class="done content"><div class="left content_left"><input class="checkbox_task" id="checkbox_active' + count + '" type="checkbox" checked><label for="checkbox_active' + count + '"></label></div>'
+                            + '<div class="middle conent_middle"></div>'
+                            + '<div class="right content_right">' + '<div class="task_text">' + result[i].task + '</div>' + '<img class="trash_can" src="assets/trash-can-active.png"></div></li>';
                         count++;
                     }
 
                     container.innerHTML += task_get;
 
                 }
-                //console.log("got it!");
+
             }
+
         });
-    }        
+
+    }
 
 
     //post task to server
-    function postTask(){
+    function postTask() {
 
         let data = {};
         data.task = addTaskInput.value;
@@ -196,25 +182,19 @@ window.onload=function(){
 
         $.ajax({
             type: 'POST',
-            headers: {"Content-Type": "application/json"},
-            url: 'http://localhost:3000/tasks',	
-            data: JSON.stringify(data),                     					
-            success: function(res) {
-                // console.log(res);
-                // console.log(JSON.stringify(data));
-                // console.log('Succes')
-            },
-            error: function(res, err){
-                // console.log(res);
-                // console.log(err);
+            headers: { "Content-Type": "application/json" },
+            url: 'http://localhost:3000/tasks',
+            data: JSON.stringify(data),
+            error: function (err) {
+                console.log(err);
             }
         });
     }
-    
+
     changeBtnColor();
     removeTask();
     checkIfEmpty();
     addDoneClass();
-    getTask();  
+    getTask();
 
 }
